5a0e922a3539185374a2f0afa54ffc82d39398d6
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-11-11 17:24:37 +0500

    deleted clients

--- clients/tcp_client.py
+++
@@ -1,9 +0,0 @@
-import socket
-import json
-from pprint import pprint
-
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 53000))
-client_socket.sendto(input().encode(), ('127.0.0.1', 53000))
-resp = client_socket.recv(8192)
-pprint(json.loads(resp))
